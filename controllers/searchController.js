feeb6b39f107ab1364bdc9eea3b36223a4050dba
Ben F <56423163+bferriman@users.noreply.github.com>
2020-04-16 18:33:08 -0600

    removed console log statements

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -2,10 +2,8 @@ const axios = require("axios");
 
 module.exports = {
   searchByQuery: function(req, res) {
-    console.log("https://www.googleapis.com/books/v1/volumes?q=" + req.params.q + "&key=" + process.env.API_KEY);
     axios.get("https://www.googleapis.com/books/v1/volumes?q=" + req.params.q + "&key=" + process.env.API_KEY)
     .then(booksData => {
-      console.log(booksData.data.items);
       res.json(booksData.data.items)
     })
     .catch(err => res.status(422).json(err));
